9acb6f10b2acc7101fd82c701700a751ff642ab3
Abel Kalum <chumbabel@gmail.com>
2020-06-26 14:54:07 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 1) do
+ActiveRecord::Schema.define(version: 2020_06_26_145342) do
+
+  create_table "figures", force: :cascade do |t|
+    t.string "name"
+  end
 
   create_table "landmarks", force: :cascade do |t|
     t.string "name"

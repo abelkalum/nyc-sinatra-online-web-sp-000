563d7a576200899570f2483e957d401cf6b35012
Abel Kalum <chumbabel@gmail.com>
2020-06-26 14:36:33 +0000

    Automatically backed up by Learn

--- 01_create_landmarks.rb
+++ 01_create_landmarks.rb
@@ -1,9 +1,3 @@
-class CreateLandmarks < ActiveRecord::Migration[5.2]
-  def change
-    create_table :landmarks do |t|
-      t.string :name
-      t.integer :figure_id
-      t.integer :year_completed
-    end
-  end
+class CreateLandmarks < ActiveRecord::Migration
+  raise 'Write CreateLandmarks migration here'
 end
